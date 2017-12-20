f1768e68d14f121e79b7b523c720cbae7ae0fc2b
xhh <18734184215@163.com>
2017-12-20 13:25:47 +0800

    RxJava操作符的完善

--- app/src/main/java/com/example/xhh/rxjavaoperatorsproject/NewActivity.java
+++ app/src/main/java/com/example/xhh/rxjavaoperatorsproject/NewActivity.java
@@ -3,12 +3,24 @@ package com.example.xhh.rxjavaoperatorsproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
+
 public class NewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+            }
+        }).buffer(3,2)
+                .subscribe();
 
     }
 }
